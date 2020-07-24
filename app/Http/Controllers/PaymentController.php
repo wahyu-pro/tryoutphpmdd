a17322cc9b647c7cf1c1bb425da246ce73051468
Wahyu Wibowo <wahyuoxs135@gmail.com>
2020-07-24 20:48:22 +0700

    1.1

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -47,54 +47,54 @@ class PaymentController extends Controller
 
     public function create(Request $request)
     {
-        // $payment = new Payment();
-        // $payment->payment_type = $request->input('data.attributes.payment_type');
-        // $payment->gross_amount = $request->input('data.attributes.gross_amount');
-        // // $payment->bank = $request->input('data.attributes.bank');
-        // $payment->transaction_id = 1243556;
-        // $payment->transaction_time = time();
-        // $payment->transaction_status = "Pending";
-        // $payment->order_id = $request->input('data.attributes.order_id');
-        // $payment->save();
-        // $id_payment = $payment->id;
+        $payment = new Payment();
+        $payment->payment_type = $request->input('data.attributes.payment_type');
+        $payment->gross_amount = $request->input('data.attributes.gross_amount');
+        // $payment->bank = $request->input('data.attributes.bank');
+        $payment->transaction_id = 1243556;
+        $payment->transaction_time = time();
+        $payment->transaction_status = "Pending";
+        $payment->order_id = $request->input('data.attributes.order_id');
+        $payment->save();
+        $id_payment = $payment->id;
 
-        // // Log::info('PaymentControllerMethodCreate');
-        // // return response()->json(['message' => "Add payment success"], 201);
+        // Log::info('PaymentControllerMethodCreate');
+        // return response()->json(['message' => "Add payment success"], 201);
 
 
-        // $paymentFind = Payment::find($id_payment);
-        // $item_list = array();
-        // $amount = 0;
-        // Config::$serverKey = 'SB-Mid-server-XNmbljytrWjbZS9Civ_JLQIh';
-        // if (!isset(Config::$serverKey)) {
-        //     return "Please set your payment server key";
-        // }
-        // Config::$isSanitized = true;
+        $paymentFind = Payment::find($id_payment);
+        $item_list = array();
+        $amount = 0;
+        Config::$serverKey = 'SB-Mid-server-XNmbljytrWjbZS9Civ_JLQIh';
+        if (!isset(Config::$serverKey)) {
+            return "Please set your payment server key";
+        }
+        Config::$isSanitized = true;
 
         // Enable 3D-Secure
-        // Config::$is3ds = true;
+        Config::$is3ds = true;
 
-        // $items = OrderItem::where('order_id', $paymentFind->order_id)->get();
+        $items = OrderItem::where('order_id', $paymentFind->order_id)->get();
         // return $items;
         // Required
-        // $item_list[] = $items;
+        $item_list[] = $items;
         // $item_list[] = [
         //     'id' => "111",
         //     'price' => 20000,
         //     'quantity' => 1,
         //     'name' => "Majohn"
         // ];
-        // $transaction_details = array(
-        //     'order_id' => rand(),
-        //     'gross_amount' => 20000, // no decimal allowed for creditcard
-        // );
+        $transaction_details = array(
+            'order_id' => $paymentFind->order_id,
+            'gross_amount' => 0, // no decimal allowed for creditcard
+        );
 
-        // // Optional
-        // $item_details = $item_list;
-        // // Optional
-        $order = Order::where('order_id', 1)->get();
+        // Optional
+        $item_details = $item_list;
+        // Optional
+        $order = Order::find($paymentFind->order_id);
         $customer = Customer::find($order->user_id);
-        return $order;
+        $customer_details = $customer;
         // $customer_details = array(
         //     'first_name'    => "Andri",
         //     'last_name'     => "Litani",
@@ -102,26 +102,26 @@ class PaymentController extends Controller
         //     'phone_number'  => "081122334455"
         // );
 
-        // // Optional, remove this to display all available payment methods
-        // $enable_payments = array();
+        // Optional, remove this to display all available payment methods
+        $enable_payments = array();
 
-        // // Fill transaction details
-        // $transaction = array(
-        //     // 'enabled_payments' => $enable_payments,
-        //     'transaction_details' => $transaction_details,
-        //     'customer_details' => $customer_details,
-        //     'item_details' => $item_details,
-        // );
-        // // return $transaction;
-        // try {
-        //     $snapToken = Snap::getSnapToken($transaction);
-        //     // return response()->json($snapToken);
-        //     return response()->json($snapToken);
-        //     // return ['code' => 1 , 'message' => 'success' , 'result' => $snapToken];
-        // } catch (\Exception $e) {
-        //     dd($e);
-        //     return ['code' => 0, 'message' => 'failed'];
-        // }
+        // Fill transaction details
+        $transaction = array(
+            // 'enabled_payments' => $enable_payments,
+            'transaction_details' => $transaction_details,
+            'customer_details' => $customer_details,
+            'item_details' => $item_details,
+        );
+        // return $transaction;
+        try {
+            $snapToken = Snap::getSnapToken($transaction);
+            // return response()->json($snapToken);
+            return response()->json($snapToken);
+            // return ['code' => 1 , 'message' => 'success' , 'result' => $snapToken];
+        } catch (\Exception $e) {
+            dd($e);
+            return ['code' => 0, 'message' => 'failed'];
+        }
     }
 
     // public function get_items($id)
